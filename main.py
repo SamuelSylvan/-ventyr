b236b04eea95b4377e85f2436b5a0c66606c6462
Samuel Sylvan <samuel.sylvan@gmail.com>
2021-02-10 17:12:33 +0000

    started projekt

--- main.py
+++ main.py
@@ -8,21 +8,40 @@ with open("äventyr.test.txt") as file:
       äventyr [rad] = information 
 
 print ("Nu börjar " + äventyr[0] +". Håll i hatten." )
-
-poiton = 1
+text = "hej"
+position = 1
 Slut = False
 while not Slut:
-  
+  text = äventyr [position]
+  text = text.split (";")
+  if text [0] == "S":
+    print (text[1])
+    Slut = True
+  if text [0] == "H":
+    print (text[1])
+    position = int (text [2])
+  if text [0] == "V":
+    print (text[1])
 
-for y in äventyr:
-  line = line.split (";")
-  if line [0] == "S":
-    print (line[1])
-    break
+   
 
-for x in äventyr:
-  line = line.split (";")
-  if line [0] == "H":
-    print (line[1])
+# 'information = information.split(";")
+  # if information [0] == "S":
+  #   print (information[1])
+  #   Slut = True
+  # if line [0] == "H":
+  #   print (line[1])
+
+# for y in äventyr:
+#   line = line.split (";")
+#   if line [0] == "S":
+#     print (line[1])
+#     break
+
+# for x in äventyr:
+#   line = line.split (";")
+#   if line [0] == "H":
+#     print (line[1])
 
 
+# '
